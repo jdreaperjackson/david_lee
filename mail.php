171db04fb7482c1a61c8e3a4a19252bada0556ed
Kenny Zhang <dzhang33@CougarNet.UH.EDU>
2022-10-20 20:31:50 -0500

    first commit

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$to 		= 'youremail.com';
+$headers	= 'FROM: "'.$email.'"';
+
+//All form values
+$name 		= $_POST['name'];
+$phone 	= $_POST['phone'];
+$email 		= $_POST['email'];
+$msg 		= $_POST['msg'];
+$output 	= 	"Name: ".$name."\nEmail: ".$email."\nPhone: ".$phone."\n\nMessage: ".$msg;
+
+$send		= mail($to, $name, $output, $headers);
